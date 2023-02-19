12c047e946f03903088825df92ee774b7cffaca0
LuanaYellow <luana@yellowpatito.com>
2023-02-19 16:28:06 -0300

    form control de procesos done

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -27,6 +27,7 @@ function Menu() {
             <Nav.Link eventKey="link-18" href="/recepcion">Recepción</Nav.Link>
             <Nav.Link eventKey="link-19" href="/descongelamiento">Descongelamiento</Nav.Link>
             <Nav.Link eventKey="link-20" href="/rechazo-mp">Reporte de Rechazo/ Devolución de Materias Primas</Nav.Link>
+            <Nav.Link eventKey="link-21" href="/control-procesos">Control de Procesos</Nav.Link>
 
 
 
