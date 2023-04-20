4318ae1378a599e34fca787277f8176c51c11817
MauriAlcalde <maurialcalde2020@gmail.com>
2023-04-20 18:07:18 -0300

    armado de reducer y actions reporte incidentes

---
+++ src/redux/actions/reporteIncidentesActions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const reporteIncidentesActions = {
+  logIn: (inputsValues) => {
+    return async (dispatch, getState) => {
+      try {
+        const response = await axios.post('http://localhost:4000/api//flashincidente', inputsValues)
+        if (response.data.success === false) {
+          alert("Usuario incorrecto")
+        }
+        if (!response.data.success) {
+          return response.data
+        }
+        dispatch({ type: 'POST_FORM', payload: response.data })
+
+      } catch (err) {
+        alert("Hubo un error, reintenta mas tarde.")
+      }
+    }
+  }}
+export default reporteIncidentesActions
--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import reporteIncidentesReducer from "./reporteIncidentesReducer";
 
 const rootReducer = combineReducers({
     userR: userReducer,
+    reporteIncidentesR:reporteIncidentesReducer,
     
 })
 
