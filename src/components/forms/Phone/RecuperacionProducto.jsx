72e6c0ef4da8ca5939dc980b90d5285d53ee5939
MauriAlcalde <maurialcalde2020@gmail.com>
2023-04-20 23:05:27 -0300

    agregado de actions y reducers y conexion con el backend

--- src/components/forms/Phone/RecuperacionProducto.jsx
+++ src/components/forms/Phone/RecuperacionProducto.jsx
@@ -1,9 +1,13 @@
 import { Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
 import styles from './RecuperacionProducto.module.css'
+import { useSelector,useDispatch } from 'react-redux'
+import recuperacionProductoActions from '../../../redux/actions/recuperacionProductoActions'
 
 function RecuperacionProducto() {
-
+    const dispatch = useDispatch()
+    const prueba = useSelector(state=>state.recuperacionProductoR.inputsValues)
+    console.log("esto anda?",prueba)
     const [values,setValues] = useState({
         fechaAlerta:"",
         fechaRecuperacion:"",
@@ -11,9 +15,10 @@ function RecuperacionProducto() {
         producto:"",
         marca:"",
         loteVencimiento:"",
-        cantidadProducto:"",
-        destinoProducto:"",
-        fechaDispFinal:""
+        cantidad:"",
+        destino:"",
+        fechaDisposicion:"",
+        idUser:"643ea98d5b44dd9765966ae7"
     })
 
     return (
@@ -39,14 +44,14 @@ function RecuperacionProducto() {
                     <TextField onChange={(e)=>{setValues({...values,loteVencimiento:e.target.value})}} id="outlined-basic" label="Lote/Vencimiento" variant="outlined" />
                 </div>
                 <div className={styles.personal}>
-                    <TextField onChange={(e)=>{setValues({...values,cantidadProducto:e.target.value})}} id="outlined-basic" label="Cantidad de Producto" variant="outlined" />
-                    <TextField onChange={(e)=>{setValues({...values,destinoProducto:e.target.value})}} id="outlined-basic" label="Destino del Producto" variant="outlined" />
-                    <TextField onChange={(e)=>{setValues({...values,fechaDispFinal:e.target.value})}} id="outlined-basic" label="Fecha de disposición final" variant="outlined" />
+                    <TextField onChange={(e)=>{setValues({...values,cantidad:e.target.value})}} id="outlined-basic" label="Cantidad de Producto" variant="outlined" />
+                    <TextField onChange={(e)=>{setValues({...values,destino:e.target.value})}} id="outlined-basic" label="Destino del Producto" variant="outlined" />
+                    <TextField onChange={(e)=>{setValues({...values,fechaDisposicion:e.target.value})}} id="outlined-basic" label="Fecha de disposición final" variant="outlined" />
                 </div>
 
                 <div className="btn">
                     <Button onClick={()=>{
-                        console.log(values)}} variant="contained">Generar PDF</Button>
+                        dispatch(recuperacionProductoActions.logIn(values))}} variant="contained">Generar PDF</Button>
 
                 </div>
 
