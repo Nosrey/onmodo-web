64cc389221a17be41ba7d7f0384056f4a4de5bf2
IgnacioVivas <ignacab@hotmail.com>
2023-04-25 17:29:57 -0300

    validando btn de login

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import { applyMiddleware } from "redux"
-import { createStore } from 'redux'
-import thunk from "redux-thunk"
-import rootReducer from './redux/reducers/rootReducer'
-import 'remixicon/fonts/remixicon.css'
+import { Provider } from 'react-redux';
+import { applyMiddleware } from 'redux';
+import { createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers/rootReducer';
+import 'remixicon/fonts/remixicon.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
