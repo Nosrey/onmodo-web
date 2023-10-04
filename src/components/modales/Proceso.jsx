b4b9bef47fdff5fa13ecaaa77b855d807c3ae398
LuanaYellow <luana@yellowpatito.com>
2023-10-04 16:07:18 -0300

    planilla control de ´roceso actualizada

--- src/components/modales/Proceso.jsx
+++ src/components/modales/Proceso.jsx
@@ -7,21 +7,42 @@ function ProcesosInfo() {
         
         <>
             <div className={styles.subtitleCont}>
-                <p className={styles.subtitle}> COCCIÓN</p>
+                <p className={styles.subtitle}>TEMPERATURAS CRÍTICAS Y ACCIONES CORRECTIVAS PARA LA COCCIÓN</p>
             </div>
-            <div className={styles.limites}>
-                <img src={imageCoccion} className={styles.limitesImg} />
+            <div>
+            <b>LÍMITE CRÍTICO</b>
+                <ul>
+                 <li>Carne vacuna, cerdo, cordero: Mayor o igual 65°C .</li>
+                 <li>Pollo y otras aves de corral: Mayor o igual a 74°C .</li>
+                 <li>Pescado: Mayor o igual a 63°C .</li>
+                 <li>Pescado: Mayor o igual a 63°C .</li>
+                 <li>Pastas rellenas: Mayor o igual a 74°C.</li>
+                 <li>Huevos y alimentos preparados: Mayor o igual a 74°C.</li>
+                 
+                 </ul>
+                 <br />
+                 <b>ACCIONES DE CORRECCIÓN </b>
+                <ol>
+                    <li>Continuar la cocción.</li>
+                    <li>Si no se alcanza la temperatura descartar el alimento.</li>
+                </ol>
+                
             </div>
             <br />
             <br />
             <br />
 
             <div className={styles.subtitleCont}>
-                <p className={styles.subtitle}>ENFRIAMIENTO</p>
+                <p className={styles.subtitle}>TEMPERATURAS CRÍTICAS Y ACCIONES CORRECTIVAS PARA EL ENFRIAMIENTO</p>
             </div>
             <b>LÍMITE CRÍTICO</b>
-            <p>TEMP. INTERNA: de 60º a 21º en 2 hs.,y luego a 4ºC en 4 hs. siguientes.</p>
-            <p>Contratos certificados con IRAM BPM: de 60º a 10º en 2 hs.,y luego debe almacenarse a 4ºC</p>
+            <p> Temperatura interna: </p>
+            <ul>
+                <li>
+                En las primeras 2 horas: menor a 21ºC.
+                </li>
+                <li>Luego, en las siguientes 4 horas:  menor a 6ºC.</li>
+            </ul>
             
             <b>ACCIONES DE CORRECCIÓN </b>
             <ul>
@@ -31,36 +52,28 @@ function ProcesosInfo() {
                     <li> Si se cumplen las temperaturas, ninguna otra acción es requerida.</li>
                     <li> Si no se cumplen, descartar el alimento.</li>
                 </ul>
-                <li>Si pasadas las 6 hs. no se llega a 4ºC, desechar.</li>
-                <p>Contratos certificados con IRAM BPM:</p>
-                <li>Si el alimento no llega a 10ºC pasadas las 2 hs., refrigerar.</li>
-                <li> Si no se cumple, regenerar a más de 75ºC y volver a realizar el enfriado.</li>
-                <ul>
-                    <li>Si se cumplen las temperaturas, ninguna otra acción es requerida.</li>
-                    <li> Si no se cumplen, descartar el alimento.</li>
-                    <li> Si pasadas las 6 hs. no se llega a 4ºC, desechar.</li>
-                </ul>
+                <li>Si pasadas las 6 hs. no se llega a 6ºC, desechar.</li>
+                
             </ul>
                 <br />
                 <br />
                 <br />
                 <div className={styles.subtitleCont}>
-                <p className={styles.subtitle}> REGENERACIÓN Y MANT. EN CALIENTE</p>
+                <p className={styles.subtitle}>TEMPERATURAS CRÍTICAS Y ACCIONES CORRECTIVAS PARA LA REGENERACIÓN Y MANT. EN CALIENTE</p>
             </div>
             <b>LÍMITE CRÍTICO</b>
-            <p>Regeneración: TEMP. INTERNA: Superior a 74ºC.</p>
-            <p>Contratos certificados con IRAM BPM: Superior a 75°C en una hora</p>
-            <p>Mantenimiento en caliente:TEMP. INTERNA:Superior a 65ºC.</p>
+            <p>Regeneración: Temperatura interna superior a 74ºC.</p>
+            <p>Mantenimiento en caliente:Temperatura interna superior a 65ºC.</p>
             
             <b>ACCIONES DE CORRECCIÓN </b>
-            <p>Regeneración:Si el alimento no se ha retermatilizado correctamente:</p>
+            <p>Regeneración:</p>
             <ul>
-                <li>Prolongar el tiempo de calentamiento hasta llegar a las temperaturas correctas (74ºC).</li>
-                <li> Si no se logra, descartar.</li>
+                <li>Si el alimento no se ha retermatilizado correctamente prolongar el tiempo de calentamiento hasta llegar a 74ºC.</li>
+                <li>Si no se logra, descartar.</li>
             </ul>
             <p>Mantenimiento en caliente:</p>
             <ul>
-                <li>Regenerar a 74ºC.</li>
+                <li>Mantener por encima de 60 ºC.</li>
                 <li> Si no se logra, descartar.</li>
             </ul>
         </>
